116605d90f3fa55d01e6343fc4e2315776081431
c7x43t <c7x43t@gmail.com>
2019-04-18 20:59:14 +0200

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,13 @@
+function binarySearch(array, value) {
+  	var j = 0, length = array.length;
+    while (j < length) {
+    	var i = (length + j - 1) >> 1; // move the pointer to 
+      if (value > array[i]) 
+        j = i + 1;
+      else if (value < array[i]) 
+        length = i;
+      else
+        return i
+    }
+  return -1
+}
