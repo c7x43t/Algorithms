8d370f1d18966a335f1952d6da13fe78873ad5d6
c7x43t <c7x43t@gmail.com>
2019-04-21 02:33:39 +0200

    Update ip_subnet.js

--- ip_subnet.js
+++ ip_subnet.js
@@ -1,6 +1,6 @@
 // example usage: new cidr("192.168.100.14/22")
 function intToSubnetMask(int){
-	var subnetMask=[0,0,0,0]
+	var subnetMask=[0,0,0,0];
 	for(i=0;i<4;i++){
 		if(int>7){
 			subnetMask[i]=255;
@@ -16,14 +16,14 @@ function cidr(ip){
 	var tmp=ip.split("/");
 	this.ip=tmp[0].split(".").map(e=>+e);
 	this.subnetMask=intToSubnetMask(+tmp[1]);
-	this.subnet=this.ip.map((e,i)=>e&this.subnetMask[i])
+	this.subnet=this.ip.map((e,i)=>e&this.subnetMask[i]);
 	this.start=ipToDec(this.subnet);
 	this.range=subnetMaskToDec(this.subnetMask);
 	this.end=this.start+this.range;
 }
 function ipToDec(ip){
-	return ip.reduce((acc,e,i)=>acc+e*256**(3-i),0)
+	return ip.reduce((acc,e,i)=>acc+e*256**(3-i),0);
 }
 function subnetMaskToDec(subnet){
-	return subnet.reduce((acc,e,i)=>acc+(255-e)*2**(8*(3-i)),0)
+	return subnet.reduce((acc,e,i)=>acc+(255-e)*2**(8*(3-i)),0);
 }
