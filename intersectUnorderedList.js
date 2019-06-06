84c106e2f11b9593dc691319b10c7e21a73ce7fe
c7x43t <c7x43t@gmail.com>
2019-06-06 20:04:57 +0200

    Update intersectUnorderedList.js

--- intersectUnorderedList.js
+++ intersectUnorderedList.js
@@ -1,27 +1,25 @@
-// fast unordered lists intersection with time complexity O(m*n) for the case of 2 arrays
-// takes one argument: array of arrays that should be intersected
-function intersectUnorderedLists(arrays){
-	if(arrays.length>1){
-		let sortedArrays=arrays.sort((a,b)=>a.length>b.length?1:-1);
-		let intersection=[];
-		for(let j=0;j<sortedArrays[0].length;j++){
-			if(sortedArrays[1].indexOf(arrays[0][j])>-1) intersection.push(arrays[0][j]);
+// fast unordered lists intersection with time complexity O(m+n) for the case of 2 arrays
+// example: intersectUnorderedLists([1,2,3],[3,4,5]) => [3]
+// performance: execution time linear with respect to the sum of the lengths of input arrays (loops exacly once trough every array): O(n)
+function intersectUnorderedLists(){
+	var shortest=0;
+	// find shortest
+	for(let i=1;i<arguments.length;i++) if(arguments[i].length < arguments[shortest].length) shortest=i;
+	// create initial lookup table
+	var obj={};
+	for(let i=0;i<arguments[shortest].length;i++) obj[arguments[shortest][i]]=arguments[shortest][i];
+	var lastIndex=arguments.length-1;
+	for(var i=0;i<lastIndex;i++){
+		if(i===shortest) continue;
+		newObj={}
+		for(var j=0;j<arguments[i].length;j++){
+			if(obj[arguments[i][j]]!==undefined) newObj[arguments[i][j]]=arguments[i][j];
 		}
-		var shortArr; var longArr;
-		for(let i=2;i<sortedArrays.length-1;i++){
-			if(sortedArrays[i].length>intersection.length){
-				shortArr=intersection;
-				longArr=sortedArrays[i];
-			}else{
-				shortArr=sortedArrays[i];
-				longArr=intersection;
-			}
-			for(let j=0;j<shortArr.length;j++){
-				if(longArr.indexOf(shortArr[j])>-1) intersection.push(shortArr[j]);
-			}
-		}
-		return intersection;
-	}else{
-		return arrays.length===1?arrays[0].map(e=>e):[];
+		obj=newObj;
 	}
+	var result=[];
+	for(var j=0;j<arguments[lastIndex].length;j++){
+		if(obj[arguments[lastIndex][j]]!==undefined) result.push(arguments[i][j]);
+	}
+	return result;
 }
