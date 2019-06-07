069884c79e75a9f4ffdc66ff3054e1c633a3a5b3
c7x43t <c7x43t@gmail.com>
2019-06-07 17:35:17 +0200

    Update fastLoop.js

--- fastLoop.js
+++ fastLoop.js
@@ -1,5 +1,7 @@
 // fastest way of looping trough an array: caching the length and scoping the length to prevent gc
 // this is useful for long running loops where the condition is checked frequently (array.length>~10)
+// and only if the loop itself is entered frequently to leverage the gc prevention: e.g. the inner part of a nested loop
+// or loops in frequently called functions
 const func=(function(){
   let len;
   return function(args){
