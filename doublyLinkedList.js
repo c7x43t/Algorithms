0e0f2bfe12624ef584e70a94653fddcfd117fc04
c7x43t <c7x43t@gmail.com>
2019-06-09 19:36:43 +0200

    Update doublyLinkedList.js

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,20 +1,20 @@
-function linkedElement(value,previous,next){
+function linkedElement(value,previous=null,next=null){
 	this.value=value;
 	this.replace=function(value){
 		var newElement=new linkedElement(value,this.previous,this.next);
-		if(this.previous!==undefined) this.previous.next=newElement;
-		if(this.next!==undefined) this.next.previous=newElement;
+		if(this.previous!==null) this.previous.next=newElement;
+		if(this.next!==null) this.next.previous=newElement;
 		return newElement;
 	}
 	this.previous=previous;
 	this.next=next;
 }
 function linkedList(){
-	this.first=undefined;
-	this.last=undefined;
+	this.first=null;
+	this.last=null;
 	this.append=function(value){
 		var newElement=new linkedElement(value,this.last);
-		if(this.last!==undefined){
+		if(this.last!==null){
 			this.last.next=newElement;
 		}else{
 			this.first=newElement;
@@ -23,7 +23,7 @@ function linkedList(){
 	}
 	this.prepend=function(value){
 		var newElement=new linkedElement(value,undefined,this.first);
-		if(this.first!==undefined){
+		if(this.first!==null){
 			this.first.previous=newElement;
 		}else{
 			this.last=newElement;
