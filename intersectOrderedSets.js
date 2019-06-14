b0c22285f4527328aece6cd39bbc9eb79281583c
c7x43t <c7x43t@gmail.com>
2019-06-14 15:14:48 +0200

    Update intersectOrderedSets.js

--- intersectOrderedSets.js
+++ intersectOrderedSets.js
@@ -1,14 +1,14 @@
 // O(2n) worst, O(n) best, where n is the total lengths of the input arrays
 // and the worst case is identical arrays and in the best cause arrays have no common elements
-function intersectOrderedSets(sets){
+function intersectOrderedSets(){
 	var shortest=0;
-	for(var i=1;i<sets.length;i++) if(sets[i].length<sets[shortest].length) shortest=i;
-	var result=sets[shortest];
-	for(var i=0,a,b,j,k;i<sets.length;i++){
+	for(var i=1;i<arguments.length;i++) if(arguments[i].length<arguments[shortest].length) shortest=i;
+	var result=arguments[shortest];
+	for(var i=0,a,b,j,k;i<arguments.length;i++){
 		if(result.length===0) return result; 
 		if(i===shortest) continue;
 		a=result;
-		b=sets[i];
+		b=arguments[i];
 		j=0;
 		k=0;
 		result=[];
