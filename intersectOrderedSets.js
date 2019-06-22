ae2c86909ba9b9da417e519561461fe29f23c7e6
c7x43t <c7x43t@gmail.com>
2019-06-23 00:38:08 +0200

    Update intersectOrderedSets.js

--- intersectOrderedSets.js
+++ intersectOrderedSets.js
@@ -8,6 +8,61 @@
 // or the more propable complexity of O(n+n+n*log2(m/n)), if n<m
 // based on https://cs.stackexchange.com/a/65866 describing following paper:
 // http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.419.8292
+function intersect2OrderedSets(a, b) {
+	let result=[];
+    let i=0;
+	let j=0;
+    let a_len=a.length;
+	let b_len=b.length;
+    // trying to minimize false branch predictions by assuming
+    // that a.len() < b.len() and there are only a few matching values (~<50%)
+    while(true) { 
+        if (a[i] > b[j]) {     
+            while (j < b_len && a[i] > b[j]) {
+                j+=2;
+            }
+            if (j >= b_len) {
+                break;
+            }
+            if (a[i] <= b[j-1]) {
+                j-=1;
+            }
+        } else if (a[i] < b[j]) {
+            while (i < a_len && a[i] < b[j]) {
+                i+=2;
+            }
+            if (i >= a_len) {
+                break;
+            }
+            if (a[i-1] >= b[j]) {
+                i-=1;
+            }
+        } else { // &a[i] == &b[j]
+            result.push(a[i]);
+            i+=1;
+            j+=1;
+            if (i == a_len || j == b_len) {
+                break;
+            }
+        }
+    }
+    return result;
+}
+function intersectOrderedSets() {
+    var shortest = 0;
+    for (var i = 1; i < arguments.length; i++)
+        if (arguments[i].length < arguments[shortest].length) shortest = i;
+    var result = arguments[shortest];
+    for (var i = 0, a, b, j, k, ratio, index; i < arguments.length; i++) {
+        if (result.length === 0) return result;
+        if (i === shortest) continue;
+        a = result;
+        b = arguments[i];
+        result = intersect2OrderedSets(a, b);
+    }
+    return result;
+}
+/* Old intersection:
 function binarySearch(array, value, j = 0, length = array.length) {
     while (j < length) {
         var i = (length + j - 1) >> 1; // move the pointer to
@@ -96,4 +151,4 @@ function intersectOrderedSets() {
     }
     return result;
 }
-
+*/
