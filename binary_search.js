a75d08c2786337d63a7b0635b2e08d7bc142539d
c7x43t <c7x43t@gmail.com>
2019-06-23 00:25:05 +0200

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,4 +1,5 @@
 // source: https://gist.github.com/kidGodzilla/92508419261b98ea6a30
+// performance ~24.39mio ops
 function binarySearch(array, value, j=0, length=array.length) {
     while (j < length) {
     	var i = (length + j - 1) >> 1; // move the pointer to 
@@ -11,5 +12,22 @@ function binarySearch(array, value, j=0, length=array.length) {
     }
   return -1
 }
+// source: https://github.com/weihanglo/rust-algorithm-club/blob/master/src/searching/binary_search/mod.rs
+// performance ~26.67mio ops
+function binarySearch(arr,target,base=0,size=arr.length){
+    while (size > 1) {
+        // mid: [base..size)
+        var half = ~~(size / 2);
+        var mid = base + half;
+        if (arr[mid] <= target) {
+            base = mid
+        }
+        size -= half;
+    }
+	return arr[base] === target?base:-1;
+}
+
+
 
 // other notable algorithm: compactBS from: https://stackoverflow.com/questions/22697936/binary-search-in-javascript/44981570#44981570
+// compactBS appears to be slower searching a 1mio sorted array
