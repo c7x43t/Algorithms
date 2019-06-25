c6f6e6198c25ed6ff50db368d960e3dec1c4501c
c7x43t <c7x43t@gmail.com>
2019-06-25 13:28:22 +0200

    Update sort.js

--- sort.js
+++ sort.js
@@ -127,6 +127,123 @@ var sort=(function(){
          return (v.length<1e3?heap3Sort:quickmiddleSort)(v);
     }
 }());
+// quick middlesort with inlined functions and unrolled recursion
+function quickmiddleSort(array) {
+	var left=0;
+	var right=array.length-1;
+	var mid;
+	var pivot;
+	var partLeft;
+	var partRight;
+	// QUICKSORT START
+	while(true){
+		// PARTITION START
+		partLeft=left;
+		partRight=right;
+		pivot = array[(partLeft + partRight) >>> 1];
+		while (partLeft <= partRight) {
+			while (array[partLeft] < pivot) { partLeft++; }
+			while (array[partRight] > pivot) { partRight--; }
+			if (partLeft <= partRight) {
+				var temp = array[partLeft];
+				array[partLeft++] = array[partRight];
+				array[partRight--] = temp;
+			}
+		}
+        mid=partLeft;
+		// PARTITION END
+		if (left < mid - 1) {
+			right=mid-1;
+			continue;
+		}
+		if (right > mid) {
+			left=mid;
+			continue;
+		}
+		break;
+	}
+	// QUICKSORT END
+	return array;
+};
+
+// heap 3 sort with inlined functions
+function heap3Sort(ary) {
+  // heapify
+  var len = ary.length;
+  var shiftStart, shiftEnd;
+  var root, child, s, root21;
+  for (var start = (len >>> 1) - 1; start >= 0; start--) {
+	  // SHIFT START
+	  root=start;
+	  shiftEnd=len-1;
+	  //shiftDown(ary, start, len - 1);
+	  while ((root21 = (root << 1) + 1) <= shiftEnd) {
+		  child = root21;
+		  s = root;
+  
+		  if (ary[s] < ary[child]) {
+			  s = child;
+		  }
+ 
+		  var child1 = child + 1;
+		  if (child1 <= shiftEnd && ary[s] < ary[child1]) {
+			  s = child1;
+		  }
+  
+		  if (s !== root) {
+			  // SWAP START
+			  //swap(ary, root, s);
+			  var tmp=ary[root];
+			  ary[root]=ary[s];
+			  ary[s]=tmp;
+			  // SWAP END
+			  root = s;
+		  } else {
+			  break;
+		  }
+	  }
+	  // SHIFT END           
+  }
+  for (var end = len - 1; end > 0; end--) {
+	  //swap(ary, end, 0);
+	  // SWAP START
+	  var tmp=ary[end];
+	  ary[end]=ary[0];
+	  ary[0]=tmp;
+	  // SWAP END
+	  // SHIFT START
+	  root=0;
+	  shiftEnd=end-1;
+	  //shiftDown(ary, 0, end - 1);
+	  while ((root21 = (root << 1) + 1) <= shiftEnd) {
+		  child = root21;
+		  s = root;
+  
+		  if (ary[s] < ary[child]) {
+			  s = child;
+		  }
+ 
+		  var child1 = child + 1;
+		  if (child1 <= shiftEnd && ary[s] < ary[child1]) {
+			  s = child1;
+		  }
+  
+		  if (s !== root) {
+			  // SWAP START
+			  //swap(ary, root, s);
+			  var tmp=ary[root];
+			  ary[root]=ary[s];
+			  ary[s]=tmp;
+			  // SWAP END
+			  root = s;
+		  } else {
+			  break;
+		  }
+	  }
+	  // SHIFT END
+  }
+  return ary;
+};
 /*
 minified:
 var sort=function(){var k=function(){function f(b,c,e){for(var a,d;(d=(c<<1)+1)<=e;)if(a=d,d=c,b[d]<b[a]&&(d=a),a+=1,a<=e&&b[d]<b[a]&&(d=a),d!==c){a=b;var f=d,g=a[c];a[c]=a[f];a[f]=g;c=d}else break}return function(b){for(var c=b.length,e=(c>>>1)-1;0<=e;e--)f(b,e,c-1);for(--c;0<c;c--){e=b;var a=e[c];e[c]=e[0];e[0]=a;f(b,0,c-1)}return b}}(),l=function(){function f(b,c,e){var a=c;for(var d=e,h=b[a+d>>>1];a<=d;){for(;b[a]<h;)a++;for(;b[d]>h;)d--;if(a<=d){var g=b[a];b[a++]=b[d];b[d--]=g}}c<a-1&&f(b,c,
