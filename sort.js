3937b3ec91546a15d5781f4d7beaa86db436099e
c7x43t <c7x43t@gmail.com>
2019-06-25 14:33:43 +0200

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,110 +1,105 @@
-// fastest sort algorithm for arrays of numbers combining heap sort and quick sort algorithms
-// significantly faster than Array.prototype.sort
-var sort=(function(){
-    var heap3Sort = (function(){
-        /*jshint bitwise: false*/
-        "use strict";
-      function swap(ary, a, b) {
-          var t = ary[a];
-          ary[a] = ary[b];
-          ary[b] = t;
-      }
-      
-      function shiftDown(ary, start, end) {
-          var root = start,
-              child, s, root21;
-      
-          while ((root21 = (root << 1) + 1) <= end) {
-              child = root21;
-              s = root;
-      
-              if (ary[s] < ary[child]) {
-                  s = child;
-              }
-     
-              var child1 = child + 1;
-              if (child1 <= end && ary[s] < ary[child1]) {
-                  s = child1;
-              }
-      
-              if (s !== root) {
-                  swap(ary, root, s);
-                  root = s;
-              } else {
-                  return;
-              }
-          }
-      }
-    
-     
-      return function heap3Sort(ary) {
-          // heapify
-          var len = ary.length;
-          for (var start = (len >>> 1) - 1; start >= 0; start--) {
-              shiftDown(ary, start, len - 1);
-          }
-          for (var end = len - 1; end > 0; end--) {
-              swap(ary, end, 0);
-              shiftDown(ary, 0, end - 1);
-          }
-          return ary;
-      };
-     
-    })();
-    var quickmiddleSort = (function(){
-        /*jshint bitwise: false*/
-        "use strict";
-    
-        /**
-        * Quicksort algorithm. It's with complexity O(n log(n)).
-        * In this version of quicksort I use the middle element of the
-        * array for pivot.
-        */
-    
-    
-        /**
-        * Quicksort algorithm
-        *
-        * @public
-        * @param {array} array Array which should be sorted.
-        * @return {array} Sorted array.
-        */
-    
-        /**
-        * Partitions the array in two parts by the middle elements.
-        * All elemnts which are less than the chosen one goes left from it
-        * all which are greater goes right from it.
-        *
-        * @param {array} array Array which should be partitioned
-        * @param {number} left Left part of the array
-        * @param {number} right Right part of the array
-        * @return {number}
-        */
-        function partition(array, left, right) {
-            var pivot = array[(left + right) >>> 1];
-            while (left <= right) {
-                while (array[left] < pivot) { left++; }
-                while (array[right] > pivot) { right--; }
-                if (left <= right) {
-                    var temp = array[left];
-                    array[left++] = array[right];
-                    array[right--] = temp;
+var sort = (function() {
+    // heap 3 sort with inlined functions
+    function heap3Sort(ary) {
+        // heapify
+        var len = ary.length;
+        var shiftStart, shiftEnd;
+        var root, child, s, root21;
+        for (var start = (len >>> 1) - 1; start >= 0; start--) {
+            // SHIFT START
+            root = start;
+            shiftEnd = len - 1;
+            //shiftDown(ary, start, len - 1);
+            while ((root21 = (root << 1) + 1) <= shiftEnd) {
+                child = root21;
+                s = root;
+
+                if (ary[s] < ary[child]) {
+                    s = child;
+                }
+
+                var child1 = child + 1;
+                if (child1 <= shiftEnd && ary[s] < ary[child1]) {
+                    s = child1;
+                }
+
+                if (s !== root) {
+                    // SWAP START
+                    //swap(ary, root, s);
+                    var tmp = ary[root];
+                    ary[root] = ary[s];
+                    ary[s] = tmp;
+                    // SWAP END
+                    root = s;
+                } else {
+                    break;
                 }
             }
-            return left;
+            // SHIFT END           
         }
-    
-        /**
-        * Recursively calls itself with different values for
-        * left/right part of the array which should be processed
-        *
-        * @private
-        * @param {array} array Array which should be processed
-        * @param {number} left Left part of the array which should be processed
-        * @param {number} right Right part of the array which should be processed
-        */
+        for (var end = len - 1; end > 0; end--) {
+            //swap(ary, end, 0);
+            // SWAP START
+            var tmp = ary[end];
+            ary[end] = ary[0];
+            ary[0] = tmp;
+            // SWAP END
+            // SHIFT START
+            root = 0;
+            shiftEnd = end - 1;
+            //shiftDown(ary, 0, end - 1);
+            while ((root21 = (root << 1) + 1) <= shiftEnd) {
+                child = root21;
+                s = root;
+
+                if (ary[s] < ary[child]) {
+                    s = child;
+                }
+
+                var child1 = child + 1;
+                if (child1 <= shiftEnd && ary[s] < ary[child1]) {
+                    s = child1;
+                }
+
+                if (s !== root) {
+                    // SWAP START
+                    //swap(ary, root, s);
+                    var tmp = ary[root];
+                    ary[root] = ary[s];
+                    ary[s] = tmp;
+                    // SWAP END
+                    root = s;
+                } else {
+                    break;
+                }
+            }
+            // SHIFT END
+        }
+        return ary;
+    };
+
+    var quickmiddleSort = (function() {
         function quicksort(array, left, right) {
-            var mid = partition(array, left, right);
+            //var mid = partition(array, left, right);
+            // PARTITION START
+            var partLeft = left;
+            var partRight = right;
+            var pivot = array[(partLeft + partRight) >>> 1];
+            while (partLeft <= partRight) {
+                while (array[partLeft] < pivot) {
+                    partLeft++;
+                }
+                while (array[partRight] > pivot) {
+                    partRight--;
+                }
+                if (partLeft <= partRight) {
+                    var temp = array[partLeft];
+                    array[partLeft++] = array[partRight];
+                    array[partRight--] = temp;
+                }
+            }
+            var mid = partLeft;
+            // PARTITION END
             if (left < mid - 1) {
                 quicksort(array, left, mid - 1);
             }
@@ -112,140 +107,45 @@ var sort=(function(){
                 quicksort(array, mid, right);
             }
         }
-    
+
         /**
-        * Quicksort's initial point
-        * @public
-        */
+         * Quicksort's initial point
+         * @public
+         */
         return function quickmiddleSort(items) {
             quicksort(items, 0, items.length - 1);
             return items;
         };
-    
+
     }());
-    return function sort(v){
-         return (v.length<1e3?heap3Sort:quickmiddleSort)(v);
+    
+	return function sort(v) {
+        return (v.length < 1e3 ? heap3Sort : quickmiddleSort)(v);
     }
 }());
-// quick middlesort with inlined functions and unrolled recursion
-function quickmiddleSort(array) {
-	var left=0;
-	var right=array.length-1;
-	var mid;
-	var pivot;
-	var partLeft;
-	var partRight;
-	// QUICKSORT START
-	while(true){
-		// PARTITION START
-		partLeft=left;
-		partRight=right;
-		pivot = array[(partLeft + partRight) >>> 1];
-		while (partLeft <= partRight) {
-			while (array[partLeft] < pivot) { partLeft++; }
-			while (array[partRight] > pivot) { partRight--; }
-			if (partLeft <= partRight) {
-				var temp = array[partLeft];
-				array[partLeft++] = array[partRight];
-				array[partRight--] = temp;
-			}
-		}
-        mid=partLeft;
-		// PARTITION END
-		if (left < mid - 1) {
-			right=mid-1;
-			continue;
-		}
-		if (right > mid) {
-			left=mid;
-			continue;
-		}
-		break;
-	}
-	// QUICKSORT END
-	return array;
-};
-
-// heap 3 sort with inlined functions
-function heap3Sort(ary) {
-  // heapify
-  var len = ary.length;
-  var shiftStart, shiftEnd;
-  var root, child, s, root21;
-  for (var start = (len >>> 1) - 1; start >= 0; start--) {
-	  // SHIFT START
-	  root=start;
-	  shiftEnd=len-1;
-	  //shiftDown(ary, start, len - 1);
-	  while ((root21 = (root << 1) + 1) <= shiftEnd) {
-		  child = root21;
-		  s = root;
-  
-		  if (ary[s] < ary[child]) {
-			  s = child;
-		  }
- 
-		  var child1 = child + 1;
-		  if (child1 <= shiftEnd && ary[s] < ary[child1]) {
-			  s = child1;
-		  }
-  
-		  if (s !== root) {
-			  // SWAP START
-			  //swap(ary, root, s);
-			  var tmp=ary[root];
-			  ary[root]=ary[s];
-			  ary[s]=tmp;
-			  // SWAP END
-			  root = s;
-		  } else {
-			  break;
-		  }
-	  }
-	  // SHIFT END           
-  }
-  for (var end = len - 1; end > 0; end--) {
-	  //swap(ary, end, 0);
-	  // SWAP START
-	  var tmp=ary[end];
-	  ary[end]=ary[0];
-	  ary[0]=tmp;
-	  // SWAP END
-	  // SHIFT START
-	  root=0;
-	  shiftEnd=end-1;
-	  //shiftDown(ary, 0, end - 1);
-	  while ((root21 = (root << 1) + 1) <= shiftEnd) {
-		  child = root21;
-		  s = root;
-  
-		  if (ary[s] < ary[child]) {
-			  s = child;
-		  }
- 
-		  var child1 = child + 1;
-		  if (child1 <= shiftEnd && ary[s] < ary[child1]) {
-			  s = child1;
-		  }
-  
-		  if (s !== root) {
-			  // SWAP START
-			  //swap(ary, root, s);
-			  var tmp=ary[root];
-			  ary[root]=ary[s];
-			  ary[s]=tmp;
-			  // SWAP END
-			  root = s;
-		  } else {
-			  break;
-		  }
-	  }
-	  // SHIFT END
-  }
-  return ary;
-};
-/*
-minified:
-var sort=function(){var k=function(){function f(b,c,e){for(var a,d;(d=(c<<1)+1)<=e;)if(a=d,d=c,b[d]<b[a]&&(d=a),a+=1,a<=e&&b[d]<b[a]&&(d=a),d!==c){a=b;var f=d,g=a[c];a[c]=a[f];a[f]=g;c=d}else break}return function(b){for(var c=b.length,e=(c>>>1)-1;0<=e;e--)f(b,e,c-1);for(--c;0<c;c--){e=b;var a=e[c];e[c]=e[0];e[0]=a;f(b,0,c-1)}return b}}(),l=function(){function f(b,c,e){var a=c;for(var d=e,h=b[a+d>>>1];a<=d;){for(;b[a]<h;)a++;for(;b[d]>h;)d--;if(a<=d){var g=b[a];b[a++]=b[d];b[d--]=g}}c<a-1&&f(b,c,
-a-1);e>a&&f(b,a,e)}return function(b){f(b,0,b.length-1);return b}}();return function(f){return 1E3>f.length?k(f):l(f)}}()
-*/
+// Benchmark:
+var ars=[
+	new Array(1e4).fill().map(Math.random),
+	new Array(1e3).fill().map(Math.random),
+	new Array(5e2).fill().map(Math.random),
+	new Array(1e2).fill().map(Math.random),
+	new Array(10).fill().map(Math.random),
+	new Array(1e5).fill().map(Math.random),
+	//new Array(1e4).fill().map(Math.random)
+];
+var ns=[2.5e3,4e4,1e5,1.2e6,2e7,2e2];
+var t1=performance.now();
+var s;
+var n, ar;
+for(var j=0;j<6;j++){
+ar=ars[j];
+n=ns[j];
+for(var i=0;i<n;i++){
+	sort(ar.slice(0));
+}
+}
+var t2=performance.now();
+console.log(t2-t1);
+// Minified
+var sort=function(){function h(c){for(var d=c.length,f,e,a,b,g=(d>>>1)-1;0<=g;g--)for(e=g,f=d-1;(b=(e<<1)+1)<=f;)if(a=b,b=e,c[b]<c[a]&&(b=a),a+=1,a<=f&&c[b]<c[a]&&(b=a),b!==e)a=c[e],c[e]=c[b],c[b]=a,e=b;else break;for(--d;0<d;d--)for(a=c[d],c[d]=c[0],c[0]=a,e=0,f=d-1;(b=(e<<1)+1)<=f;)if(a=b,b=e,c[b]<c[a]&&(b=a),a+=1,a<=f&&c[b]<c[a]&&(b=a),b!==e)a=c[e],c[e]=c[b],c[b]=a,e=b;else break;return c}var k=function(){function c(d,f,e){for(var a=f,b=e,g=d[a+b>>>1];a<=b;){for(;d[a]<g;)a++;for(;d[b]>g;)b--;if(a<=
+b){var h=d[a];d[a++]=d[b];d[b--]=h}}f<a-1&&c(d,f,a-1);e>a&&c(d,a,e)}return function(d){c(d,0,d.length-1);return d}}();return function(c){return(1E3>c.length?h:k)(c)}}();
